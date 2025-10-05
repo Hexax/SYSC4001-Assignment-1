f2c09fb58fb856709e636bec1b07a274b4b3bdd4
hexax <mickaelroy@cmail.carleton.ca>
2025-10-05 18:28:28 -0400

    Added documentation for lambda function, Reworked logic for system calls and end_io so that it works with syscalls that are shorter then 40 cycles // added final line to see clocks final time. // changed time taken to execute trace file to the top of the outputfile

--- interrupts.cpp
+++ interrupts.cpp
@@ -31,6 +31,18 @@ int main(int argc, char **argv) {
   // variable used to help calculate remaining time for IO devices.
   int isr_total_delays = 0;
 
+  //
+  /**
+   * \brief Simulates one trace file line execution and logs it.
+   *
+   * Lambda function that logs the execution of a trace file. It passes by reference execution and clock
+   *
+   * @param duration Time needed to comlete the action
+   * @param action Descriptive string that documents what the OS is doing.
+   * @param incr_isr_total_delays Set to true when calling the function if duration == ISR_DELAY constant.
+   * @return a vector of strings (the parsed vector table)
+   *
+   */
   auto simcycles = [&](int duration, const std::string &action, bool incr_isr_total_delays = false) {
     // log -> current clock, duration of interrupt, action performed
     execution += std::to_string(clock) + ", " + std::to_string(duration) + ", " + action + "\n";
@@ -38,6 +50,7 @@ int main(int argc, char **argv) {
     // add duration of interrupt to current clock
     clock += duration;
 
+    // Optionally increment isr_total_delays to track the time already spent on an isr.
     if (incr_isr_total_delays) {
       isr_total_delays++;
     }
@@ -64,6 +77,7 @@ int main(int argc, char **argv) {
     execution += executiontemp;
     clock = tempclock;
 
+    // Error checking to ensure there is a device associated with the interrupt
     if (duration_intr < 0 || duration_intr >= static_cast<int>(delays.size())) {
       simcycles(1, "ERROR: invalid device id " + std::to_string(duration_intr));
       simcycles(1, "IRET");
@@ -71,27 +85,41 @@ int main(int argc, char **argv) {
     }
 
     if (activity == "SYSCALL") {
-      // Run the ISR
-      simcycles(ISR_DELAY, "SYSCALL: run the ISR (device driver)", true);
 
-      // transfer data from device to memory
-      simcycles(ISR_DELAY, "transfer data from device to memory", true);
+      // Time taken to run the ISR
+      int firststep = std::min(ISR_DELAY, delays[duration_intr] / 2);
 
-      if (delays[duration_intr] - (ISR_DELAY * isr_total_delays) > 0) {
-        simcycles(delays[duration_intr] - (ISR_DELAY * isr_total_delays), "Remaining ISR tasks");
-      }
+      // Time taken to transfer data from device to memory
+      int secondstep = std::min(ISR_DELAY, delays[duration_intr] - firststep);
+
+      // Remaining time to complete ISR tasks
+      int laststep = delays[duration_intr] - firststep - secondstep;
+
+      if (firststep > 0)
+        simcycles(firststep, "SYSCALL: run the ISR (device driver)", true);
+      if (secondstep > 0)
+        simcycles(secondstep, "transfer data from device to memory", true);
+      if (laststep > 0)
+        simcycles(laststep, "Remaining ISR tasks");
 
       // Return to user mode after interrupt
       simcycles(1, "IRET");
+      continue;
+    }
+
+    if (activity == "END_IO") {
 
       // END_IO Simulation
-    } else if (activity == "END_IO") {
+      int firststep = std::min(ISR_DELAY, delays[duration_intr]);
 
-      simcycles(ISR_DELAY, "END_IO: Store information in memory", true);
+      // Remaining time to complete ISR tasks
+      int laststep = delays[duration_intr] - firststep;
 
-      if (delays[duration_intr] - (ISR_DELAY * isr_total_delays) > 0) {
-        simcycles(delays[duration_intr] - (ISR_DELAY * isr_total_delays), "check device status");
-      }
+      if (firststep > 0)
+        simcycles(firststep, "END_IO: Store information in memory", true);
+
+      if (laststep > 0)
+        simcycles(laststep, "check device status");
 
       // Return to user mode after interrupt
       simcycles(1, "IRET");
@@ -99,10 +127,12 @@ int main(int argc, char **argv) {
     /************************************************************************/
   }
 
-  // simulate CPU speeds and log results at end of output file
+  // simulate CPU speeds and log results at the top of output file
   double totaltime = static_cast<double>(clock) / CPU_SPEED;
-  execution += "Total time taken to execute trace file: " + std::to_string(totaltime) + " seconds";
+  execution = "Total time taken to execute trace file: " + std::to_string(totaltime) + " seconds\n" + execution;
 
+  // add final line to logging file.
+  execution += std::to_string(clock) + ", NULL, NO MORE INSTRUCTIONS CPU SLEEP";
   input_file.close();
 
   write_output(execution);
